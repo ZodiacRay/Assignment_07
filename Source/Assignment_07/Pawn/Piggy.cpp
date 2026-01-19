8271bcba026bfee6f7a805ec87bb70145a801b0f
minsung <99423006+zarame3636@users.noreply.github.com>
2026-01-19 16:41:47 +0900

    과제완료

---
+++ Source/Assignment_07/Pawn/Piggy.cpp
@@ -0,0 +1,201 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Piggy.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+// Sets default values
+APiggy::APiggy()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	SetRootComponent(SphereCollision);
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(SphereCollision);
+	Mesh->SetSimulatePhysics(false);
+	Mesh->SetEnableGravity(true); 
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(SphereCollision);
+	CameraBoom->TargetArmLength = 200.f;
+	CameraBoom->bUsePawnControlRotation = false;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom);
+	FollowCamera->SetRelativeRotation(FRotator(0.f, -20.f, 0.f));
+
+	
+}
+
+void APiggy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
+
+	PreviousLocation = GetActorLocation();
+}
+
+void APiggy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UpdateSpeed(DeltaTime);
+
+	CheckGround(); 
+
+	if (bIsFalling)
+	{
+		VerticalVelocity += GravityConstant * DeltaTime;
+
+		FVector VerticalMove = FVector(0, 0, VerticalVelocity * DeltaTime);
+		AddActorLocalOffset(VerticalMove, true);
+	}
+
+}
+
+// Called to bind functionality to input
+void APiggy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComp->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APiggy::Move);
+		EnhancedInputComp->BindAction(MoveUpAction, ETriggerEvent::Triggered, this, &APiggy::MoveUp);
+		EnhancedInputComp->BindAction(MoveDownAction, ETriggerEvent::Triggered, this, &APiggy::MoveDown);
+		EnhancedInputComp->BindAction(LookAction, ETriggerEvent::Triggered, this, &APiggy::Look);
+		EnhancedInputComp->BindAction(LookRollAction, ETriggerEvent::Triggered, this, &APiggy::LookRoll);
+		EnhancedInputComp->BindAction(ZoomAction, ETriggerEvent::Triggered, this, &APiggy::Zoom);
+	}
+}
+
+void APiggy::Move(const FInputActionValue& Value)
+{
+	if (!Controller) return;
+
+	FVector2D MoveVector = Value.Get<FVector2D>(); 
+
+	const FVector ForwardDir = GetActorForwardVector();
+	const FVector RightDir = GetActorRightVector();
+
+	FVector DeltaLocation = ForwardDir * MoveVector.Y + RightDir * MoveVector.X;
+	float DelatTime = GetWorld()->GetDeltaSeconds(); 
+
+	float FinalSpeed = bIsFalling ? MoveSpeed * AirMovementRegist : MoveSpeed;
+
+	AddActorWorldOffset(DeltaLocation * FinalSpeed * DelatTime, true);
+}
+
+void APiggy::MoveUp(const FInputActionValue& Value)
+{
+	if (!Controller) return;
+
+	AddActorLocalOffset(FVector::UpVector * MoveSpeed * GetWorld()->GetDeltaSeconds(), true);
+	VerticalVelocity = 0; 
+}
+
+void APiggy::MoveDown(const FInputActionValue& Value)
+{
+	if (!Controller || !bIsFalling) return; 
+
+	AddActorLocalOffset(FVector::DownVector * MoveSpeed * GetWorld()->GetDeltaSeconds(), true);
+}
+
+void APiggy::Look(const FInputActionValue& Value)
+{
+	if (!Controller) return;
+
+	FVector2D LookVector = Value.Get<FVector2D>();
+	
+	AddActorLocalRotation(FRotator(0.f, LookVector.X, 0.f));
+
+	if (CameraBoom)
+	{
+		FRotator NewArmRotation = CameraBoom->GetRelativeRotation();
+	
+		NewArmRotation.Pitch = FMath::Clamp(NewArmRotation.Pitch + (LookVector.Y * -1.f), -60.f, 60.f);
+		CameraBoom->SetRelativeRotation(NewArmRotation);
+	}
+
+}
+
+void APiggy::LookRoll(const FInputActionValue& Value)
+{
+	if (!Controller || !CameraBoom) return; 
+
+	float RollValue = Value.Get<float>(); 
+
+	FRotator NewArmRotation = CameraBoom->GetRelativeRotation();
+	CurrentRoll = (CurrentRoll + (RollValue * 2.f));
+	NewArmRotation.Roll = CurrentRoll; 
+
+	CameraBoom->SetRelativeRotation(NewArmRotation);
+}
+
+void APiggy::Zoom(const FInputActionValue& Value)
+{
+	if (!Controller) return; 
+
+	float ZoomAmount = Value.Get<float>(); 
+	CameraBoom->TargetArmLength += ZoomAmount * ZoomStep;
+}
+
+void APiggy::CheckGround()
+{
+	FHitResult HitResult;
+	FVector Start = GetActorLocation();
+	FVector End = Start + FVector::DownVector * (SphereCollision->GetScaledSphereRadius() + 3);
+
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+
+	bool bHit = GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		Start, End, 
+		ECC_Visibility,
+		Params
+	);
+
+	if (bHit)
+	{
+		bIsFalling = false; 
+		VerticalVelocity = 0.f;
+	}
+	else
+	{
+		bIsFalling = true;
+	}
+}
+
+void APiggy::UpdateSpeed(float DeltaTime)
+{
+	FVector CurrentLocation = GetActorLocation();
+
+	FVector Displacement = CurrentLocation - PreviousLocation;
+	float DistanceMoved = Displacement.Size();
+	float Speed = DistanceMoved / DeltaTime;
+
+	FVector MovedDir = Displacement.GetUnsafeNormal();
+	float DotDir = FVector::DotProduct(GetActorForwardVector(), MovedDir); 
+
+
+	CurrentSpeed = DotDir >= 0 ? Speed : -Speed; 
+
+	PreviousLocation = CurrentLocation;
+}
---
+++ Source/Assignment_07/Pawn/Piggy.h
@@ -0,0 +1,121 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Piggy.generated.h"
+
+
+class USkeletalMeshComponent; 
+class USphereComponent; 
+class USpringArmComponent; 
+class UCameraComponent; 
+class UInputAction;
+class UInputMappingContext; 
+struct FInputActionValue; 
+
+UCLASS()
+class ASSIGNMENT_07_API APiggy : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APiggy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void Move(const FInputActionValue& Value);
+	void MoveUp(const FInputActionValue& Value);
+	void MoveDown(const FInputActionValue& Value);
+
+	void Look(const FInputActionValue& Value);
+	void LookRoll(const FInputActionValue& Value);
+
+
+	void Zoom(const FInputActionValue& Value); 
+
+
+protected:
+
+	void CheckGround(); 
+
+	bool IsFalling() { return bIsFalling; }
+
+	void UpdateSpeed(float DeltaTime);
+
+private: 
+
+	// Components 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (allowprivateaccess = true), Category = "Component")
+	USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (allowprivateaccess = true), Category = "Component")
+	USphereComponent* SphereCollision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (allowprivateaccess = true), Category = "Component")
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (allowprivateaccess = true), Category = "Component")
+	UCameraComponent* FollowCamera;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+
+	// Input Actions 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveUpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveDownAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookRollAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* ZoomAction;
+
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MoveSpeed = 600.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	float CurrentSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsFalling = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float AirMovementRegist = 0.4f;
+
+protected: 
+	FVector PreviousLocation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Zoom")
+	float ZoomStep = 200.f;
+
+	float CurrentRoll = 0.f;
+
+	float VerticalVelocity = 0.f; 
+
+	const float GravityConstant = -980.f;
+};
+
